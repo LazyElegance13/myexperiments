e83419fb55201c39508b70b16d7d203d3c28a06c
LazyElegance13 <piyushthefallenangel@gmail.com>
2016-09-20 08:35:20 +0530

    Udacity Web App files added

---
+++ projectpi-udacity-1/Archives/Testfile.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 19 13:19:25 2016
+
+@author: Lucifer_Angel
+"""
+
+#<form method="post" action="/testform">
+<form> 
+   <input type="password" name="q">
+    <input type="submit">
+</form>
